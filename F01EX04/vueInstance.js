be37ad3f5fab99c6b509877aeb94a2b9a25d0dd6
Ricardo Queirós <ricardo.queiros@gmail.com>
2020-10-13 22:23:54 +0100

    Exercise sheet 01 solved

--- F01EX04/vueInstance.js
+++ F01EX04/vueInstance.js
@@ -4,6 +4,7 @@ const vm = new Vue({
         productName: 'Computador Titan W59',
         productDesc: 'Processador i7<br>Disco SSD 256GB<br> Monitor Samsung 21’',
         productImage: 'https://www.casasbahia-imagens.com.br/Informatica/Computadores/13863246/1313975776/computador-easypc-standard-intel-core-i7-38ghz-8gb-hd-3tb-monitor-195-windows-10-pacote-office-13863246.jpg',
-        productPrice: 1200
+        productPrice: 1200,
+        productAvailability: true
     }
 })
