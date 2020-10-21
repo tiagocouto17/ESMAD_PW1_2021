5281def9bdb86614596a578fbe05242a18077e3e
Ricardo Queirós <ricardo.queiros@gmail.com>
2020-10-21 12:20:27 +0100

    Finished exercise sheet #04

--- F04EX03/vueInstance.js
+++ F04EX03/vueInstance.js
@@ -2,15 +2,29 @@ const vm = new Vue({
     el: '#app',
     data: {
         teams: [
-            { name: 'benfica', year: 1910 },
-            { name: 'porto', year: 1912 },
-            { name: 'sporting', year: 1914 },
-        ]
+            { name: 'benfica', year: 1910, location:'lisboa' },
+            { name: 'fcp', year: 1912, location:'porto' },
+            { name: 'sporting', year: 1914, location:'lisboa' },
+        ],
+        location:''
     },
     methods: {
-        addTeam(teamName, teamYear) {
-            const newTeam = { name: teamName, year: teamYear }
+        addTeam(teamName, teamYear, teamLocation) {
+            const newTeam = { 
+                name: teamName, 
+                year: teamYear, 
+                location: teamLocation }
             this.teams.push(newTeam)
+        },
+        filterLocation(newLocation) {
+            this.location = newLocation
+        }
+    },
+    computed: {
+        filteredTeams() {
+            return this.teams.filter(
+                team => team.location === this.location || this.location ==''
+            )
         }
     }
 })
