0297904a8e5fa0c2050da580d6ee26027150481e
Ricardo Queirós <ricardo.queiros@gmail.com>
2020-10-30 15:59:12 +0000

    Exercise 7 done

--- F07EX01/vueInstance.js
+++ F07EX01/vueInstance.js
@@ -6,31 +6,41 @@ const vm = new Vue({
             country: 'camaroes',
             cities: 'a,b',
             description: 'asdasd',
-            startDate: '28-10-2020',
-            endDate: '29-10-2020',
+            startDate: '',
+            endDate: '',
             type: 'holidays',
             photo: 'http://www.x.pt',
             filter: {
-                continent:'europe',
-                startdDate:'',
-                endDate:'',
-                type:''
-            }            
-        },                
-        travels: []
+                continent: '',
+                startDate: '',
+                endDate: '',
+            }
+        },
+        travels: [],
+        flagCountry:-1
     },
     created() {
-
+        if(localStorage.getItem('travels')) {
+            this.travels = JSON.parse(localStorage.getItem('travels'))
+        }
+    },
+    destroyed() {
+        localStorage.setItem('travels', JSON.stringify(this.travels))
     },
     methods: {
         getNextId() {
-            if (this.travels.length === 0) {
+            if (this.travels.length == 0) {
                 return 1
             } else {
                 return this.travels[this.travels.length - 1].id + 1
             }
         },
-
+        formatDate(d) {
+            console.log(d);
+            d = new Date(d)
+            console.log(d);
+            return `${d.getDay()}-${d.getMonth()}-${d.getFullYear()}`
+        },
         addTravel() {
             const newTravel = {
                 id: this.getNextId(),
@@ -41,23 +51,70 @@ const vm = new Vue({
                 startDate: new Date(this.frm.startDate),
                 endDate: new Date(this.frm.endDate),
                 type: this.frm.type,
-                photo: this.frm.photo,
+                photo: this.frm.photo
             }
             this.travels.push(newTravel)
         },
         removeTravel(id) {
-            if (confirm('Deseja mesmo remover a viagem?')) {
-                this.travels = this.travels.filter(
-                    travel => travel.id !== id
-                )
-            }
+            this.travels = this.travels.filter(
+                travel => travel.id != id
+            )
+        },
+        editTravel(id) {
+            const travelId = this.travels.findIndex(
+                travel => travel.id == id
+            )
+            this.travels[travelId].country = prompt('Novo país?')
+
+        },
+        sortTravelByCountry() {
+            this.flagCountry = this.flagCountry * -1
+            this.travels = this.travels.sort(this.compareCountries)
+        },
+        compareCountries(a,b) {
+            if (a.country > b.country) return 1 * this.flagCountry
+            if (a.country < b.country) return -1 * this.flagCountry
+            if (a.country == b.country) return 0
+        },
+        sortTravelByStartDate() {
+            this.travels = this.travels.sort(this.compareDates)
+        },
+        compareDates(a,b) {
+            if (a.startDate > b.startDate) return 1
+            if (a.startDate < b.startDate) return -1
+            if (a.startDate == b.startDate) return 0
         }
+
     },
     computed: {
         filteredTravels() {
             return this.travels.filter(
-                travel => travel.continent === this.frm.filter.continent || this.frm.filter.continent === ''
+                travel => {
+                    let filterContinentResult = true
+                    let filterDatesResult = true
+
+                    // Filter continent
+                    if (this.frm.filter.continent !== "") {
+                        filterContinentResult = travel.continent === this.frm.filter.continent
+                    }
+
+                    // Filter dates                    
+                    if (this.frm.filter.startDate !== "" && this.frm.filter.endDate !== "") {
+                        filterDatesResult =
+                            travel.startDate >= new Date(this.frm.filter.startDate) &&
+                            travel.startDate <= new Date(this.frm.filter.endDate)
+                    }
+
+                    // return the conjunction of the two filters
+                    return filterContinentResult && filterDatesResult
+                }
+
             )
         }
     }
 })
+
+
+window.onunload = function () {
+    vm.$destroy()    
+}
