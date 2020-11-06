28abbe7f1a0b891ed84e320bd8da5edc5425aa3a
Ricardo Queirós <ricardo.queiros@gmail.com>
2020-11-06 15:38:55 +0000

    Finished Exercise Sheet nº8

--- F08EX01/vueInstance.js
+++ F08EX01/vueInstance.js
@@ -5,23 +5,137 @@ const vm = new Vue({
             name: '',
             link: '',
             edit: {
-                id:'',
+                id: '',
                 name: '',
                 link: ''
             },
             filter: {
-                name:''
-            }            
+                name: ''
+            }
         },
-        link:'',
+        link: '',
         flagSortCastle: 1,
         castles: []
     },
     methods: {
+        manipulateArray(exercise) {
+            switch (exercise) {
+                case 'ei':
+                    const str = 'Castelo'
+                    this.castles.map(
+                        castle => {
+                            castle.name = castle.name.replace(str, '')
+                            return castle
+                        }
+                    )
+                    break;
+                case 'eii':
+                    const castlesYearAverage = this.castles.reduce(
+                        (acum, cur) => acum + cur.year, 0
+                    ) / this.castles.length
+                    console.log(castlesYearAverage);
+                    break;
+                case 'eiii':
+                    const castleName = 'Marvão'
+                    const existMarvaoCastle = this.castles.some(
+                        castle => castle.name.includes(castleName)
+                    )
+                    console.log(
+                        existMarvaoCastle ? 'SIM' : 'NÃO'
+                    );
+                    break;
+                case 'eiv':
+                    const letter = 'A'
+                    const newCastles = this.castles.filter(
+                        castle => castle.name.startsWith(letter)
+                    )
+                    console.table(newCastles);
+                    break;
+                case 'ev':
+                    const linksFulfilled = this.castles.every(
+                        castle => castle.link.length != 0
+                    )
+
+                    console.log(
+                        linksFulfilled ? "SIM" : "NÃO"
+                    );
+
+                    break;
+                case 'evi':
+                    break;
+                case 'evii':
+
+                    const castleName2 = 'Castelo de Santa Maria da Feira'
+                    const castleObj = this.castles.find(
+                        castle => castle.name === castleName2
+                    )
+                    console.log(
+                        castleObj ? 'existe esse castelo!' : 'Não existe esse castelo!'
+                    );
+                    break;
+                case 'eviii':
+                    const castleName3 = 'Castelo de Arraiolos'
+                    const castlePos = this.castles.findIndex(
+                        castle => castle.name === castleName3
+                    )
+                    console.log(castlePos)
+                    break;
+                case 'fi':
+                    const separator = '-'
+                    const castleNames = this.castles.map(
+                        castle => castle.name
+                    )
+
+                    console.log(castleNames.join(separator))
+                    break;
+                case 'fii':
+                    const castleYear = 1950
+                    const modernCastles = this.castles.filter(
+                        castle => castle.year > castleYear                    
+                    )
+                    console.table(modernCastles)
+                    break;
+                case 'fiii':
+                    const resetLinksYear = 1900
+                    const newCastlesArray = this.castles.map(
+                        castle => {
+                            if(castle.year < resetLinksYear) {
+                                castle.link = ''
+                            }
+                            return castle
+                        }
+                    )
+                    console.table(newCastlesArray)
+                    break;
+                case 'fiv':
+                    const incrementsYear = 5
+
+                    const newCastleYears = this.castles.map(
+                        castle => {
+                            if(
+                                castle.name.toLowerCase().startsWith('a') ||
+                                castle.name.toLowerCase().startsWith('e') ||
+                                castle.name.toLowerCase().startsWith('i') ||
+                                castle.name.toLowerCase().startsWith('o') ||
+                                castle.name.toLowerCase().startsWith('u')
+                            ) {
+                                castle.year += incrementsYear
+                            }
+                            return castle
+                        }
+                    )
+
+                    console.table(newCastleYears)    
+
+                    break;
+                default:
+                    break;
+            }
+        },
         getNextId() {
             // Forma abrevaiada
             // return this.castles.length ? this.castles[this.castles.length - 1] + 1 : 1
-            
+
             // Forma tradicional
             if (this.castles.length == 0) {
                 return 1
@@ -87,8 +201,8 @@ const vm = new Vue({
             if (confirm('Deseja remover o castelo?')) {
                 this.castles = this.castles.filter(
                     castle => castle.id !== id
-                )    
-            }            
+                )
+            }
         },
         sortCastles() {
             // ordenar castlos pelo nome (alterando entre ordenação crescente e decrescente)
@@ -96,9 +210,9 @@ const vm = new Vue({
             this.castles.sort(this.compareCastles)
         },
         compareCastles(a, b) {
-            if(a.name > b.name) return 1 * this.flagSortCastle
-            if(a.name < b.name) return -1 * this.flagSortCastle
-            if(a.name === b.name) return 0
+            if (a.name > b.name) return 1 * this.flagSortCastle
+            if (a.name < b.name) return -1 * this.flagSortCastle
+            if (a.name === b.name) return 0
         }
     },
     computed: {
@@ -152,19 +266,19 @@ const vm = new Vue({
             id: 7,
             name: "Castelo de Santa Maria da Feira",
             link: "https://i2.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/10993133_785137361533499_2834769696870331489_o-1.jpg?resize=640%2C415",
-            year: 1000
+            year: 1980
         },
         {
             id: 8,
             name: "Castelo de Lindoso",
             link: "https://i2.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/10854966_780229555357613_3783697791888632461_o.jpg?resize=640%2C415",
-            year: 1000
+            year: 2000
         },
         {
             id: 9,
             name: "Castelo dos Mouros",
             link: "https://i2.wp.com/www.vortexmag.net/wp-content/uploads/2015/04/casteloguim-1-e1446910277358.jpg?resize=640%2C415",
-            year: 1951
+            year: 1000
         },
         {
             id: 10,
