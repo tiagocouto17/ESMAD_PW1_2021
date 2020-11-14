4d6d59e40de4ce17dd7124a39938b7d2be222961
Ricardo Queirós <ricardo.queiros@gmail.com>
2020-11-14 11:30:10 +0000

    Exercise 4 solved

--- F09EX04/vueComponent.js
+++ F09EX04/vueComponent.js
@@ -1,11 +1,32 @@
 // Definir o componente counter-button
-Vue.component('color-picker', {
+Vue.component('game-soccer-card', {
+    props: {
+        game : {
+            type: Object,
+            required: true
+        }
+    },
     template: `
-        <input 
-            type='color'
-            v-on:change='$emit("change-color",$event.target.value)'
-        >
-    `
+    <div class="card" style="width: 18rem;">
+        <img :src="game.stadiumLink" class="card-img-top" :alt="game.stadiumName">
+        <div class="card-body">
+            <p class="card-title">
+                {{game.teamName1}} {{this.getTeamGoals(game.teamName1)}}
+                 - 
+                 {{this.getTeamGoals(game.teamName2)}} {{game.teamName2}}
+            </p>
+            <p class="card-text"><b>Estádio:</b> {{game.stadiumName}}</p>
+            <p class="card-text" v-for='goal in game.goals'>
+                {{goal.goalMinute}}' {{goal.playerName}} {{goal.playerTeam}}
+            </p>
+        </div>
+    </div>
+    `,
+    methods: {
+        getTeamGoals(teamName) {
+            return this.game.goals.filter(goal => goal.playerTeam == teamName).length
+        }
+    }
  }
 )
 
